e7731baaa665fdeede8cee584bee5ab91b2c1b94
VIDHACH <chaitanya.vidhale@tecnotree.com>
2025-09-12 11:17:53 +0530

    loader changes

--- src/components/ui/loder.tsx
+++ src/components/ui/loder.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import aanganLogo from "@/assets/aangan-logo.png"; // make sure the path is correct
 
 const Loader: React.FC = () => {
   const loaderContainerStyle: React.CSSProperties = {
@@ -18,14 +19,26 @@ const Loader: React.FC = () => {
     border: "8px solid #f3f3f3",
     borderTop: "8px solid #3498db",
     borderRadius: "50%",
-    width: "60px",
-    height: "60px",
+    width: "80px",
+    height: "80px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     animation: "spin 1s linear infinite",
+  };
+
+  const logoStyle: React.CSSProperties = {
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    objectFit: "contain",
   };
 
   return (
     <div style={loaderContainerStyle}>
-      <div style={loaderStyle}></div>
+      <div style={loaderStyle}>
+        <img src={aanganLogo} alt="Aangan Logo" style={logoStyle} />
+      </div>
       <style>
         {`
           @keyframes spin {
